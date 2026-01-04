8a88945628d520630146fc9de500a987b703e054
Okon Jedidiah Solomon <96173434+okondev2021@users.noreply.github.com>
2026-01-05 00:12:32 +0100

    Fix catch block type annotation for error handling

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -58,7 +58,7 @@ export async function POST(request: NextRequest) {
         );
 
     }
-    catch (err as geminiErrorFormat) {
+    catch (err: geminiErrorFormat) {
         console.log(err)
         return NextResponse.json(
             { message: "Error with extracting details from document"},
