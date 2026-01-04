c50609d69622fa8b40378211e344e0d9142d9b6b
Okon Jedidiah Solomon <96173434+okondev2021@users.noreply.github.com>
2026-01-04 19:30:11 +0100

    Change modelId from 'gemini-2.5-flash' to 'gemma-3-12b'

--- utils/gemini.ts
+++ utils/gemini.ts
@@ -7,7 +7,7 @@ export const extractDataFromTranscript = async (
 
   const ai = new GoogleGenAI({ apiKey });
 
-  const modelId = "gemini-2.5-flash";
+  const modelId ="gemma-3-12b"
 
   const config = {
     responseMimeType: "application/json",
