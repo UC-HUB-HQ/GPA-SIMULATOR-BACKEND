89bb345539daa0da9d50ef97605e4b0f85a1b1d7
Okon Jedidiah Solomon <96173434+okondev2021@users.noreply.github.com>
2026-01-05 00:49:21 +0100

    Update gemini.ts

--- utils/gemini.ts
+++ utils/gemini.ts
@@ -7,7 +7,7 @@ export const extractDataFromTranscript = async (
 
   const ai = new GoogleGenAI({ apiKey });
 
-  const modelId = "gemma-3-12b";
+  const modelId = "gemma-3-1b";
 
   const config = {
     responseMimeType: "application/json",
