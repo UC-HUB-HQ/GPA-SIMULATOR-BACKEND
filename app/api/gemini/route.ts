6124774d8c4aaaee551a3a881ed37629faf440ea
Okon Jedidiah Solomon <96173434+okondev2021@users.noreply.github.com>
2026-01-05 00:14:25 +0100

    Change error type in catch block to 'any'

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -58,7 +58,7 @@ export async function POST(request: NextRequest) {
         );
 
     }
-    catch (err: geminiErrorFormat) {
+    catch (err: any) {
         console.log(err)
         return NextResponse.json(
             { message: "Error with extracting details from document"},
