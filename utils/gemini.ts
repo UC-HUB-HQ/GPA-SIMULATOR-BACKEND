511448baf392e355d4a44701c2f8b75907dadbb2
Okon Jedidiah Solomon <96173434+okondev2021@users.noreply.github.com>
2026-01-05 01:24:25 +0100

    Update modelId to use latest version

--- utils/gemini.ts
+++ utils/gemini.ts
@@ -7,7 +7,7 @@ export const extractDataFromTranscript = async (
 
   const ai = new GoogleGenAI({ apiKey });
 
-  const modelId = "gemini-2.0-flash-lite";
+  const modelId = "gemini-flash-lite-latest";
 
   const config = {
     responseMimeType: "application/json",
